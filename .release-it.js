46556a3ebe75816d67d4fb02de722f418ceb2d75
v_wangtao34 <v_wangtao34@baidu.com>
2023-07-27 18:26:09 +0800

    ..

--- .release-it.js
+++ .release-it.js
@@ -1,10 +1,13 @@
 module.exports = {
+  // git的提交信息
   "git": {
     "commitMessage": "chore: release v${version}"
   },
+  // 发布github上的release包
   "github": {
     "release": true
   },
+  // 发布到npm
   "npm": {
     "publish": true
   },
@@ -13,6 +16,7 @@ module.exports = {
     "after:git:release": "echo git更新成功",
     "after:bump": "echo 更新版本成功 ${version}"
   },
+  // 生成changelog
   "plugins": { 
       "@release-it/conventional-changelog": { 
           "preset": "angular", 
